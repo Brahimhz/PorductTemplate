6db09638a115dd5e04e0478c3813a38165153d4a
Bouhadida Ibrahim Hamza <Bouhadidahz95@hotmail.com>
2023-11-21 19:30:28 +0400

    Login Page First Template

--- ProductUI/src/app/components/login/login.component.ts
+++ ProductUI/src/app/components/login/login.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
