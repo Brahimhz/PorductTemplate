8c79d7636e0fbf2078fa2838dc6f4c735268f2be
Bouhadida Ibrahim Hamza <Bouhadidahz95@hotmail.com>
2023-11-22 00:01:39 +0400

    Login & Signup Page UI

--- ProductUI/src/app/components/login/login.component.ts
+++ ProductUI/src/app/components/login/login.component.ts
@@ -10,4 +10,15 @@ import { RouterModule } from '@angular/router';
 })
 export class LoginComponent {
 
+
+  type: string = "password"
+  isText: boolean = false
+  eyeIcon : string  = "fa-eye-slach"
+
+  hideShowPassword(){
+
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slach";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
 }
--- ProductUI/src/app/components/signup/signup.component.ts
+++ ProductUI/src/app/components/signup/signup.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
 
+  type: string = "password"
+  isText: boolean = false
+  eyeIcon : string  = "fa-eye-slach"
+
+  hideShowPassword(){
+
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slach";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
 }
