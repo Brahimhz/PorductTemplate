99fb8d49539bc7a9b0930448bd741107402286c3
Bouhadida Ibrahim Hamza <Bouhadidahz95@hotmail.com>
2023-11-22 10:00:37 +0400

    Inputs Validation

--- ProductUI/src/app/components/login/login.component.ts
+++ ProductUI/src/app/components/login/login.component.ts
@@ -1,24 +1,106 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import ValidateForm from '../../helpers/validateform';
+
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    //private toast: NgToastService,
+    //private auth: AuthService,
+    //private router: Router,
+    //private userStore: UserStoreService,
+    //private resetPasswordService: ResetPasswordService
+    ){
+
+  }
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username : ['',Validators.required],
+      password : ['',Validators.required]
+    })
+  }
 
   type: string = "password"
   isText: boolean = false
   eyeIcon : string  = "fa-eye-slach"
 
   hideShowPassword(){
-
     this.isText = !this.isText;
-    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slach";
-    this.isText ? this.type = "text" : this.type = "password";
+    this.isText ? (this.eyeIcon = 'fa-eye') : (this.eyeIcon = 'fa-eye-slash');
+    this.isText ? (this.type = 'text') : (this.type = 'password');
   }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      // this.auth.signIn(this.loginForm.value).subscribe({
+      //   next: (res) => {
+      //     console.log(res);
+      //     this.loginForm.reset();
+      //     this.router.navigate(['dashboard']);
+      //     this.toast.success({
+      //       detail: 'Success',
+      //       summary: 'Login Success',
+      //       duration: 5000,
+      //     });
+      //     this.auth.storeToken(res.accessToken!);
+      //     this.auth.storeRefreshToken(res.refreshToken!);
+      //     let decodedValue = this.auth.decodedToken();
+      //     this.userStore.storeFullName(decodedValue.name);
+      //     this.userStore.storeRole(decodedValue.role);
+
+      //   }
+      // });
+    } else {
+      // this.toast.error({
+      //   detail: 'ERROR',
+      //   summary: 'Please fill all details',
+      //   duration: 5000,
+      // });
+      ValidateForm.validateAllFormFields(this.loginForm);
+    }
+  }
+
+  // public isValidEmail!: boolean;
+
+  // checkValidEmail(event: string) {
+  //   const value = event;
+  //   const pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  //   this.isValidEmail = pattern.test(value);
+  //   return this.isValidEmail;
+  // }
+
+  // confirmToReset() {
+  //   if (this.checkValidEmail(this.resetPasswordEmail)) {
+  //     this.resetPasswordService.sendResetPasswordLink(this.resetPasswordEmail)
+  //       .subscribe({
+  //         next: (res) => {
+  //           const buttonRef = document.getElementById("closeBtn");
+  //           buttonRef?.click();
+  //         },
+  //         error: (err) => {
+  //           this.toast.error({
+  //             detail: 'ERROR',
+  //             summary: 'Something went wrong!',
+  //             duration: 5000,
+  //           });
+  //         }
+  //       })
+  //   }
+  // }
+
+
 }
--- ProductUI/src/app/components/signup/signup.component.ts
+++ ProductUI/src/app/components/signup/signup.component.ts
@@ -1,24 +1,73 @@
-import { Component } from '@angular/core';
+//import { AuthService } from './../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+//import { NgToastService } from 'ng-angular-popup';
+import { Router, RouterModule } from '@angular/router';
+import ValidateForm from '../../helpers/validateform';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
-  type: string = "password"
-  isText: boolean = false
-  eyeIcon : string  = "fa-eye-slach"
+  public signUpForm!: FormGroup;
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon:string = "fa-eye-slash"
+  constructor(
+    private fb : FormBuilder,
+    //private toast: NgToastService,
+    //private auth: AuthService,
+    private router: Router
+    ) { }
+
+  ngOnInit() {
+    this.signUpForm = this.fb.group({
+      firstName:['', Validators.required],
+      lastName:['', Validators.required],
+      userName:['', Validators.required],
+      email:['', Validators.required],
+      password:['', Validators.required]
+    })
+  }
 
   hideShowPassword(){
-
     this.isText = !this.isText;
-    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slach";
-    this.isText ? this.type = "text" : this.type = "password";
+    this.isText ? this.eyeIcon = 'fa-eye' : this.eyeIcon = 'fa-eye-slash'
+    this.isText ? this.type = 'text' : this.type = 'password'
   }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      console.log(this.signUpForm.value);
+
+      // let signUpObj = {
+      //   ...this.signUpForm.value,
+      //   role:'',
+      //   token:''
+      // }
+      // this.auth.signUp(signUpObj)
+      // .subscribe({
+      //   next:(res=>{
+      //     console.log(res);
+      //     this.signUpForm.reset();
+      //     this.router.navigate(['login'])
+      //     this.toast.success({detail:"Success",summary:'Registration Success',duration:5000});
+      //   }),
+      //   error:(err=>{
+      //     console.log(err)
+      //     this.toast.error({detail:"ERROR",summary:err.error.message,duration:50000000});
+      //   })
+      // })
+    } else {
+      //this.toast.error({detail:"ERROR",summary:'Please fill all details',duration:5000});
+      ValidateForm.validateAllFormFields(this.signUpForm); //{7}
+    }
+  }
+
 }
