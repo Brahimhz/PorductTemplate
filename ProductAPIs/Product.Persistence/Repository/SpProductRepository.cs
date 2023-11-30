f0c429ef6c0259311590bd2e36f9a9b3df659eaa
Bouhadida Ibrahim Hamza <Bouhadidahz95@hotmail.com>
2023-11-30 12:37:22 +0400

    Extract SP names from AppSetting + check If exist

--- ProductAPIs/Product.Persistence/Repository/SpProductRepository.cs
+++ ProductAPIs/Product.Persistence/Repository/SpProductRepository.cs
@@ -25,6 +25,15 @@ namespace Product.Persistence.Repository
 
         public async Task<ProductE.Product?> GetByIdAsync(Guid id)
         {
+            //Check the existance of StoredProcuder
+
+            if (
+                _storedProducer is null ||
+                string.IsNullOrEmpty(_storedProducer.GetById) ||
+                StoredProcedureExists(_storedProducer.GetById)
+              )
+                return null;
+
             var result =
                 await _dbSet
                 .FromSqlRaw(
@@ -40,40 +49,88 @@ namespace Product.Persistence.Repository
 
         public async Task<List<ProductE.Product>> GetAllAsync()
         {
+            if (
+                _storedProducer is null ||
+                string.IsNullOrEmpty(_storedProducer.GetAllRecords) ||
+                StoredProcedureExists(_storedProducer.GetAllRecords)
+              )
+                return null;
+
             var result = await _dbSet.FromSqlRaw(_storedProducer.GetAllRecords).ToListAsync();
             return result;
         }
 
         public async Task<int> InsertAsync(ProductE.Product entity)
-        =>
-             await _context.Database.ExecuteSqlRawAsync(
-                                $"EXEC {_storedProducer.InsertProduct} @Name, @Category, @Amount, @IsActive",
-                                new SqlParameter("@Name", entity.Name),
-                                new SqlParameter("@Category", entity.Category),
-                                new SqlParameter("@Amount", entity.Amount),
-                                new SqlParameter("@IsActive", entity.isActive)
-                            );
+        {
+            if (
+                _storedProducer is null ||
+                string.IsNullOrEmpty(_storedProducer.InsertProduct) ||
+                StoredProcedureExists(_storedProducer.InsertProduct)
+              )
+                return 0;
+
+
+            return
+                await _context.Database.ExecuteSqlRawAsync(
+                    $"EXEC {_storedProducer.InsertProduct} @Name, @Category, @Amount, @IsActive",
+                    new SqlParameter("@Name", entity.Name),
+                    new SqlParameter("@Category", entity.Category),
+                    new SqlParameter("@Amount", entity.Amount),
+                    new SqlParameter("@IsActive", entity.isActive)
+                );
+        }
+
 
 
         public async Task<int> UpdateAsync(Guid id, ProductE.Product entity)
-        =>
-            await _context.Database.ExecuteSqlRawAsync(
-                $"EXEC {_storedProducer.UpdateProductById} @Id, @NewName, @NewCategory, @NewAmount, @NewIsActive",
-                new SqlParameter("@Id", id),
-                new SqlParameter("@NewName", entity.Name),
-                new SqlParameter("@NewCategory", entity.Category),
-                new SqlParameter("@NewAmount", entity.Amount),
-                new SqlParameter("@NewIsActive", entity.isActive)
-            );
+        {
+            if (
+                _storedProducer is null ||
+                string.IsNullOrEmpty(_storedProducer.UpdateProductById) ||
+                StoredProcedureExists(_storedProducer.UpdateProductById)
+              )
+                return 0;
+
+            return
+                await _context.Database.ExecuteSqlRawAsync(
+                    $"EXEC {_storedProducer.UpdateProductById} @Id, @NewName, @NewCategory, @NewAmount, @NewIsActive",
+                    new SqlParameter("@Id", id),
+                    new SqlParameter("@NewName", entity.Name),
+                    new SqlParameter("@NewCategory", entity.Category),
+                    new SqlParameter("@NewAmount", entity.Amount),
+                    new SqlParameter("@NewIsActive", entity.isActive)
+                );
+        }
+
 
 
 
 
         public async Task<int> RemoveAsync(Guid id)
-        =>
-            await _context.Database.ExecuteSqlRawAsync(
-                $"EXEC {_storedProducer.DeleteProductById} @ProductId",
-                new SqlParameter("@ProductId", id)
-            );
+        {
+            if (
+                _storedProducer is null ||
+                string.IsNullOrEmpty(_storedProducer.DeleteProductById) ||
+                StoredProcedureExists(_storedProducer.DeleteProductById)
+              )
+                return 0;
+
+            return
+                await _context.Database.ExecuteSqlRawAsync(
+                    $"EXEC {_storedProducer.DeleteProductById} @ProductId",
+                    new SqlParameter("@ProductId", id)
+                );
+        }
+
+
+
+        private bool StoredProcedureExists(string storedProcedureName)
+        {
+            var sql = $"SELECT COUNT(*) FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = 'PROCEDURE' AND ROUTINE_NAME = '{storedProcedureName}'";
+
+            var result = _context.Database.ExecuteSqlRaw(sql);
+
+            return result > 0;
+        }
     }
 }
